de06bbc2db0d1c7b8ae3adaa6a60b95df9e31c66
Gonzalo López <glnobel93@gmail.com>
2022-04-14 22:26:04 +0200

    Quicksand bold para font-weights altos

--- src/assets/fonts/fonts.js
+++ src/assets/fonts/fonts.js
@@ -1,8 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 // Quicksand
-import quicksandWoff from './quicksand/quicksand-regular.woff';
-import quicksandWoff2 from './quicksand/quicksand-regular.woff2';
+import quicksandWoff from './quicksand-regular/quicksand-regular.woff';
+import quicksandWoff2 from './quicksand-regular/quicksand-regular.woff2';
+
+import quicksandBoldWoff from './quicksand-bold/quicksand-bold.woff';
+import quicksandBoldWoff2 from './quicksand-bold/quicksand-bold.woff2';
 
 import edoWoff from './edo/edo.woff';
 import edoWoff2 from './edo/edo.woff2';
@@ -29,9 +32,18 @@ export default createGlobalStyle`
     @font-face {
         font-family: 'Quicksand';
         src: local('Quicksand'), local('Quicksand'),
-        url(${quicksandWoff2}) format('woff2'),
-        url(${quicksandWoff}) format('woff');
+        url(${quicksandBoldWoff2}) format('woff2'),
+        url(${quicksandBoldWoff}) format('woff');
         font-weight: 600;
+        font-style: normal;
+    }
+
+    @font-face {
+        font-family: 'Quicksand';
+        src: local('Quicksand'), local('Quicksand'),
+        url(${quicksandBoldWoff2}) format('woff2'),
+        url(${quicksandBoldWoff}) format('woff');
+        font-weight: 900;
         font-style: normal;
     }
 
