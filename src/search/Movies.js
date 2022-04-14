b0e8c335a2623ba02dae36a1f96d0de592c72c12
Gonzalo López <glnobel93@gmail.com>
2022-04-14 14:29:01 +0200

    Lógica modal en Movies.js

--- src/search/Movies.js
+++ src/search/Movies.js
@@ -1,23 +1,53 @@
 import React, {
-	useEffect
+	useEffect,
+	useState
 } from "react";
 import {
 	useSelector,
 	useDispatch
 } from 'react-redux'
 import { getTopRatedMovies } from '../redux/moviesSearch';
+import { addRated } from '../redux/ratedMovies';
+
 import MoviesList from "./MoviesList";
+import Modal from '../components/Modal/Modal';
 
 const Movies = () => {
+	const [modalShown, setModalShown] = useState(false);
+	const [modalData, setModalData] = useState({});
+	const [selectedMovie, setSelectedMovie] = useState();
 	const movies = useSelector((state) => state.movies.value);
 	const dispatch = useDispatch();
 
+	const getMovieFromId = (id) => {
+		const movie = movies.filter((movie) => movie.id === id);
+		return movie.length > 0 ? movie[0] : null;
+	};
 	const handleMovieClick = (id) => {
-		console.log(`Muestra el modal para la peli: ${id}`);
+		console.log(`Muestra el modal para la peli: ${id}.`);
+		const movieData = getMovieFromId(id);
+
+		if (movieData !== null) {
+			setSelectedMovie(id);
+			setModalData(movieData);
+			setModalShown(true);
+		}
 	};
-	const handleMovieRated = (id) => {
-		console.log(`Añade la película ${id} a la lista de valoradas.`);
+	const handleMovieRated = (rating) => {
+		console.log(`Añade la peli con id ${selectedMovie} a la lista de pelis valoradas.`);
+		const movieToAdd = getMovieFromId(selectedMovie);
+
+		if (movieToAdd !== null) {
+			const ratedMovie = {
+				... movieToAdd[0],
+				rating
+			};
+			dispatch(addRated(ratedMovie));
+		}
 	};
+	const handleModalClose = () => {
+		setModalShown(false);
+	}
 
 	// Cargamos las mejor valoradas para la primera carga.
 	useEffect(() => {
@@ -25,11 +55,19 @@ const Movies = () => {
 	}, []);
 
 	return (
-		<MoviesList
-			movies={movies}
-			onMovieClick={handleMovieClick}
-			onMovieRated={handleMovieRated}
-		/>
+		<div>
+			<MoviesList
+				movies={movies}
+				onMovieClick={handleMovieClick}
+				onMovieRated={handleMovieRated}
+			/>
+			{modalShown &&
+				<Modal
+					data={modalData}
+					onModalClose={handleModalClose}
+				/>
+			}
+		</div>
 	)
 }
 
