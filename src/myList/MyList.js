7dc918638abec1bf3bac560cffaf03b7d633f330
Gonzalo López <glnobel93@gmail.com>
2022-04-14 18:18:30 +0200

    Modal de consulta en MyList

--- src/myList/MyList.js
+++ src/myList/MyList.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {
+    useState
+} from "react";
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 import Movie from '../components/Movie/Movie';
+import Modal from '../components/Modal/Modal';
 
 const RatedList = styled.div`
     display: flex;
@@ -12,26 +15,52 @@ const RatedList = styled.div`
 `;
 
 const MyList = () => {
+    const [modalShown, setModalShown] = useState(false);
+	const [modalData, setModalData] = useState({});
+
     const rated = useSelector((state) => state.rated.value);
+
+    const handleMovieClick = (id) => {
+		const movieData = rated.find((movie) => movie.id === id);
+
+		if (movieData !== null) {
+			setModalData(movieData);
+			setModalShown(true);
+		}
+    }
+    const handleModalClose = () => {
+		setModalShown(false);
+	}
+
     return (
-        <RatedList>
-            {rated.length > 0
-                ? rated.map((movie) => {
-                    const   {id, poster_path, rating, title} = movie,
-                            salt = Date.now();
-                    return (
-                        <Movie
-                            id={id}
-                            image={poster_path}
-                            key={id + salt}
-                            rating={rating}
-                            title={title}
-                        />
-                    )
-                })
-                : <h1>Nihil novum sub sole</h1>
+        <>
+            <RatedList>
+                {rated.length > 0
+                    ? rated.map((movie) => {
+                        const   {id, poster_path, rating, title} = movie,
+                                salt = Date.now();
+                        return (
+                            <Movie
+                                id={id}
+                                image={poster_path}
+                                key={id + salt}
+                                onMovieClick={handleMovieClick}
+                                rating={rating}
+                                title={title}
+                            />
+                        )
+                    })
+                    : <h1>Nihil novum sub sole</h1>
+                }
+            </RatedList>
+            {modalShown &&
+                <Modal
+                    data={modalData}
+                    onModalClose={handleModalClose}
+                    readOnly={true}
+                />
             }
-        </RatedList>
+        </>
     );
 };
 
