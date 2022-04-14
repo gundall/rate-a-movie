d60590ba4f8e396c48c97da5b8b04945fcad9513
Gonzalo López <glnobel93@gmail.com>
2022-04-14 04:09:59 +0200

    Creación del Provider + Uso de prueba de Movies

---
+++ src/search/Movies.js
@@ -0,0 +1,37 @@
+import React, {
+	useEffect
+} from "react";
+import {
+	useSelector,
+	useDispatch
+} from 'react-redux'
+import { getTopRatedMovies/* , getMoviesByTitle  */} from '../redux/moviesSearch';
+import MoviesList from "./MoviesList";
+
+const Movies = () => {
+	const movies = useSelector((state) => state.movies.value);
+	const dispatch = useDispatch();
+
+	const handleMovieClick = (id) => {
+		console.log(`Muestra el modal para la peli: ${id}`);
+	};
+	const handleMovieRated = (id) => {
+		console.log(`Añade la película ${id} a la lista de valoradas.`);
+	};
+
+	// Cargamos las mejor valoradas para la primera carga.
+	useEffect(() => {
+		dispatch(getTopRatedMovies());
+		// dispatch(getMoviesByTitle('Caillou'));
+	}, []);
+
+	return (
+		<MoviesList
+			movies={movies}
+			onMovieClick={handleMovieClick}
+			onMovieRated={handleMovieRated}
+		/>
+	)
+}
+
+export default Movies;
