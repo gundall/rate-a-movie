4418aa70acaa9920d02973cf2a50c7318d48427e
Gonzalo López <glnobel93@gmail.com>
2022-04-14 23:08:57 +0200

    Centrado contenido Movie cuando tenemos estrellas

---
+++ src/myList/MyListStyled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const RatedList = styled.div`
+    display: block;
+	overflow-y: auto;
+	padding: 0 20px;
+
+	@media screen and (min-width: 477px) {
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: flex-start;
+	}
+`;
+
+export {
+    RatedList
+};
