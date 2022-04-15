440ee70592135eaf2dbc145b76091c79b05273db
Gonzalo López <glnobel93@gmail.com>
2022-04-15 13:53:00 +0200

    Animación botones menú

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,30 +1,55 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { gsap } from 'gsap';
+
 import {
 	NavContainer
 } from '../../themes/bn/MenuStyled';
 
 const Menu = () => {
+	const handleButtonHover = (e) => {
+		gsap.to(e.currentTarget, {
+			duration: 0.25,
+			scale: 0.9,
+		});
+	};
+
+	const handleButtonHoverOut = (e) => {
+		gsap.to(e.currentTarget, {
+			scale: 1
+		});
+	};
+
 	return (
 		<NavContainer>
-			<NavLink to="/search" style={({ isActive }) => {
-				return isActive
-					? {
-						backgroundColor: '#000',
-						color: '#fff'
-					}
-					: {};
-			}}>
+			<NavLink
+				onMouseEnter={handleButtonHover}
+				onMouseLeave={handleButtonHoverOut}
+				style={({ isActive }) => {
+					return isActive
+						? {
+							backgroundColor: '#000',
+							color: '#fff'
+						}
+						: {};
+				}}
+				to="/search"
+			>
 				Search
 			</NavLink>
-			<NavLink to="/myList" style={({ isActive }) => {
-				return isActive
-					? {
-						backgroundColor: '#000',
-						color: '#fff'
-					}
-					: {};
-			}}>
+			<NavLink
+				onMouseEnter={handleButtonHover}
+				onMouseLeave={handleButtonHoverOut}
+				style={({ isActive }) => {
+					return isActive
+						? {
+							backgroundColor: '#000',
+							color: '#fff'
+						}
+						: {};
+				}}
+				to="/myList"
+			>
 				My List
 			</NavLink>
 		</NavContainer>
